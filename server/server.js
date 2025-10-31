bdf457df5718f2a3039b59b8dd00eab12d8c906e
Chad Vezina <chadvezina@hotmail.com>
2025-10-31 10:14:29 -0400

    Config connexion MySql

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 
@@ -7,8 +11,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+let db;
+if (process.env.DB_HOST) {
+  db = mysql.createConnection(dbConfig);
+
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+      return;
+    }
+    console.log("Connected to the MySQL database");
+  });
+}
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
