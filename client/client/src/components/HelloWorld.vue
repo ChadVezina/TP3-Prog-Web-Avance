fa7aba7d9b6b8e33ac87c1b6703dba073e1581fa
Chad Vezina <chadvezina@hotmail.com>
2025-10-30 20:41:11 -0400

    Config Tailwind

--- client/client/src/components/HelloWorld.vue
+++ client/client/src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
+  <div class="">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
@@ -35,9 +35,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
